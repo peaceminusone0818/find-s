69c0a37f302f9bf0ac108253bdbb7a211bb93d95
anikeshk <anikesh.kamath@gmail.com>
2018-09-29 02:56:55 +0530

    Added comments

--- AnimalType/animalType.py
+++ AnimalType/animalType.py
@@ -2,6 +2,7 @@ import csv
 
 animal_dataset = []
 
+# Import the data from the CSV file
 with open('zoo.data.csv') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     for row in csv_reader:
@@ -9,17 +10,22 @@ with open('zoo.data.csv') as csv_file:
 
 #animal_dataset = [animal[1:] for animal in animal_dataset]
 
+# The attributes that describe the animals
 dataset_attributes = ['name', 'hair', 'feathers', 'eggs', 'milk', 'airborne', 'aquatic', 'predator', 'toothed',
                       'backbone', 'breathes', 'venomous', 'fins', 'legs', 'tail', 'domestic', 'catsize']
 attributes = len(animal_dataset[0])
 
 no_of_animal_types = 7
 
+# The animal types present in the CSV file
 animal_types = ['mammal', 'bird', 'reptilia', 'fish', 'amphibias', 'insect', 'other']
 
+# Creating a list that can hold the hypothesis for each of the seven animal types
 target_concept = ['x'] * (attributes-1)
 target_concept = [target_concept] * no_of_animal_types
 
+# Function that compares an attr. from a single positive training data and an attr. from the current hypothesis
+# Returns the instance attr. if 0, the same attr. if they are the same and a ? if they are different
 def finds(instance_attribute, hypotheses):
     if hypotheses == 'x':
         return instance_attribute
@@ -31,15 +37,13 @@ def finds(instance_attribute, hypotheses):
         else:
             return '?'
 
+# Runs all the training data through the map function
 for animal in animal_dataset:
     target = int(animal[attributes-1])
     target_concept[target-1] = list(map(finds, animal, target_concept[target-1]))
 
+# Displays the final hypothesis for each of the animal types
 print('The target concept for the Animal Type data set is :')
 print(str(dataset_attributes))
 for animal in range(0, no_of_animal_types):
     print('For '+animal_types[animal]+' : '+str(target_concept[animal]))
-
-
-
-
--- EnjoySport/enjoySport.py
+++ EnjoySport/enjoySport.py
@@ -2,16 +2,20 @@ import csv
 
 weather_dataset = []
 
+# Import the data from the CSV file
 with open('enjoysport.csv') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     for row in csv_reader:
         weather_dataset.append(row)
 
+# The attributes that describe the weather
 dataset_attributes = ['Sky', 'AirTemp', 'Humidity', 'Wind', 'Water', 'Forecast']
 
+# Initial hypothesis, which is the most specific hypothesis
 target_concept = ['0', '0', '0', '0', '0', '0']
 
-
+# Function that compares an attr. from a single positive training data and an attr. from the current hypothesis
+# Returns the instance attr. if 0, the same attr. if they are the same and a ? if they are different
 def finds(instance_attribute, hypotheses):
     if hypotheses == '0':
         return instance_attribute
@@ -23,9 +27,10 @@ def finds(instance_attribute, hypotheses):
         else:
             return '?'
 
-
+# Runs all the training data through the map function
 for instance in weather_dataset:
     if instance[6] == '1':
         target_concept = list(map(finds, instance, target_concept))
 
+# Displays the final hypothesis
 print('The target concept for the Enjoy Sport data set is : '+str(dataset_attributes)+' => '+str(target_concept))
